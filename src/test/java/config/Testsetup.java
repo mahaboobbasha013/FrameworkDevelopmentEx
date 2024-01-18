666d763bbcaf574f723183ad216ba053bc559e89
mahaboobbasha013 <c.mahaboobbasha74@gmail.com>
2024-01-18 14:35:51 +0530

    new changes

--- src/test/java/config/Testsetup.java
+++ src/test/java/config/Testsetup.java
@@ -1,5 +1,15 @@
 package config;
 
-public class Testsetup {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 
+public class Testsetup {
+	public static void main(String[] args) {
+		WebDriver driver=new EdgeDriver() ;
+			driver.get("https:www.google.com");
+			driver.manage().window().maximize();
+			driver.quit();
+		
+	
+	}
 }
